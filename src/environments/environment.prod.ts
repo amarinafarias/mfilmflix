c87ed2edd256726ba1d6d39302c91e61f17b4234
Marina Farias <marinafarias.mk@gmail.com>
2022-01-17 18:06:49 -0300

    Instalação inicial para o projeto filmflix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'mfilmflix',
+    appId: '1:712514196552:web:9fb24c1299a75e0331b47d',
+    storageBucket: 'mfilmflix.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD3xmn5XCpywdKo9Yrt0cs0qkU1LAmTbW4',
+    authDomain: 'mfilmflix.firebaseapp.com',
+    messagingSenderId: '712514196552',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mfilmflix',
+    appId: '1:712514196552:web:9fb24c1299a75e0331b47d',
+    storageBucket: 'mfilmflix.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD3xmn5XCpywdKo9Yrt0cs0qkU1LAmTbW4',
+    authDomain: 'mfilmflix.firebaseapp.com',
+    messagingSenderId: '712514196552',
+  },
   production: false
 };
 
